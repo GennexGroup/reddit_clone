439c26d04fb1f09fe998e9479dd949337706dcb1
Akil Kurji <akkurji@Akils-MacBook-Pro.local>
2015-09-24 14:03:58 -0500

    initial db seed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,33 +2,7 @@ class HomeController < ApplicationController
   def index
     @cool_links = []
 
-    link1 = {
-     title:'The Best Search Engine Ever',
-     url: 'http://www.google.com',
-     user: 'akkurji',
-     date: Time.now,
-     imgtype: "cats"
-    }
 
-    link2 = {
-     title:'The Worst Search Engine Ever',
-     url: 'http://www.bing.com',
-     user: 'travis',
-     date: Time.now,
-     imgtype: "business"
-    }
-
-    link3 = {
-     title:'The best promo company ever',
-     url: 'http://www.brandnex.com',
-     user: 'niti',
-     date: Time.now,
-     imgtype: "nightlife"
-    }
-
-    @cool_links.push(link1)
-    @cool_links.push(link2)
-    @cool_links.push(link3)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+link1 = Link.new({
+ title:'The Best Search Engine Ever',
+ url: 'http://www.google.com',
+ user_name: 'akkurji',
+ published_date: Time.now,
+ src_location: "cats"
+})
+
+link1.save
+
+link2 = Link.create({
+ title:'The Worst Search Engine Ever',
+ url: 'http://www.bing.com',
+ user_name: 'travis',
+ published_date: Time.now,
+ src_location: "business"
+})
+
+
+
+
+link3 = Link.create({
+ title:'The best promo company ever',
+ url: 'http://www.brandnex.com',
+ user_name: 'niti',
+ published_date: Time.now,
+ src_location: "nightlife"
+})
